ed1a86a0fdf1c4d41e5f84c30b9901e53f926567
ishan dutta <ishandutta2007@gmail.com>
2022-12-23 10:09:20 +0530

    Update comment_out_function_libclang.py

--- comment_out_function_libclang.py
+++ comment_out_function_libclang.py
@@ -25,10 +25,27 @@ def comment_out_function(input_filename, output_filename, function_name):
                 output_lines.append('// }')
             else:
                 # Add the function declaration to the output
-                output_lines.append(tu.get_extent(node.extent.start, node.extent.end).spelling)
+                output_lines.append(tu.get_extent(node.extent.start.start, node.extent.end.end).spelling)
         # Recursively visit the children of this node
         for child in node.get_children():
             visit(child)
+
+#     def visit(node):
+#         # Check if the node represents a function declaration
+#         if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+#             # Check if the function name matches the target function
+#             if node.spelling == function_name:
+#                 # Comment out the function body
+#                 output_lines.append('// {')
+#                 for i in range(node.extent.start.line + 1, node.extent.end.line):
+#                     output_lines.append('// ' + tu.get_extent(tu.get_location(input_filename, i, 0)).spelling)
+#                 output_lines.append('// }')
+#             else:
+#                 # Add the function declaration to the output
+#                 output_lines.append(tu.get_extent(node.extent.start, node.extent.end).spelling)
+#         # Recursively visit the children of this node
+#         for child in node.get_children():
+#             visit(child)
 
     # Visit the root node of the AST
     visit(tu.cursor)
